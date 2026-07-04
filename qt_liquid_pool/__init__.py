b6ac4f80f341307c46cbcd88f1093d47131f767c
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-07-04 15:15:41 +0800

    Bump version 0.0.0 -> 1.0.0

--- qt_liquid_pool/__init__.py
+++ qt_liquid_pool/__init__.py
@@ -27,7 +27,7 @@ from liquiphy import LiquidSFZ
 from qt_extras import SigBlock
 from conn_jack import JackConnectionManager, JackConnectError
 
-__version__ = "0.0.0"
+__version__ = "1.0.0"
 
 
 SYNTH_NAME = 'liquidsfz'
